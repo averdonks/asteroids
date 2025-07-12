4bbfa6c35e45731bcf987166117c68dc91087140
averdonks <62974106+averdonks@users.noreply.github.com>
2025-07-11 20:47:42 -0400

    Add code to set framerate to 60 FPS

--- main.py
+++ main.py
@@ -5,10 +5,13 @@ def main():
     # print("Starting Asteroids!")
     # print("Screen width:", SCREEN_WIDTH)
     # print("Screen height:", SCREEN_HEIGHT)
-    black = (0, 0, 0)
-    
+
     pygame.init()
+
+    black = (0, 0, 0)
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    clock = pygame.time.Clock()
+    dt = 0
 
     # Game loop
     while True:
@@ -18,6 +21,9 @@ def main():
                 return
         screen.fill(black)
         pygame.display.flip()
+
+        # Set framerate to 60 FPS
+        dt = clock.tick(60) / 1000
     
 if __name__ == "__main__":
     main()
